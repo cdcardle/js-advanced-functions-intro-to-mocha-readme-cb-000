3dce1f18bbedc191098ebec8c4d646eb380772bd
Chris Cardle <cdcardle@gmail.com>
2018-11-26 17:41:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ function theTruth() {
   return true;
 }
 
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHelloFriend(name) {
+  return `Hey, ${name}!`;
+}
+
 function favoriteIceCream(flavor) {
   return `I love ${flavor}`;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(n) {
+  return Math.floor(n);
 }
